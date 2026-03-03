a7a176dbea16f9cbe2d48e9330ddad35bac2acac
Hugo Lindstrom <huggepugge1@gmail.com>
2026-03-03 16:24:49 +0100

    Plot

--- Assignment4/pthreads/plot.py
+++ Assignment4/pthreads/plot.py
@@ -2,28 +2,37 @@ import matplotlib.pyplot as plt
 import subprocess
 
 
-x = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
-y1 = []
+particles = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+threads = [i for i in range(16)]
+y = []
+initial = float(subprocess.run([
+    "./galsim",
+    str(5000),
+    "input_data/ellipse_N_05000.gal",
+    str(100),
+    str(0.00001),
+    str(0),
+    str(1),
+], capture_output=True).stdout.decode().strip().split(': ')[1].strip())
 
-for i in x:
+y.append(1)
+
+for i in threads[1:]:
     child_output = subprocess.run([
         "./galsim",
-        str(i),
-        f"input_data/ellipse_N_{i:05}.gal",
+        str(5000),
+        "input_data/ellipse_N_05000.gal",
         str(100),
         str(0.00001),
         str(0),
-        str(10),
+        str(i),
     ], capture_output=True).stdout.decode().strip().split(': ')[1].strip()
-    y1.append(float(child_output))
+    y.append(initial / float(child_output))
 
 LW = 3.0
 
-plt.plot(x, y1, linewidth=LW, label="Our runtimes")
-
-y2 = [y1[0] * (x[i] / x[0])**2 for i in range(len(x))]
-
-plt.plot(x, y2, linewidth=LW, label="Exactly O(N^2)")
+plt.plot(threads, y, linewidth=LW, label="Improvement")
+plt.plot(threads, threads, linewidth=LW, label="Linear")
 
 plt.legend(fontsize="xx-large")
 
